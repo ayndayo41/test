6e853c758b80e7f1f5b462aa09182bed51f63b9a
a.ohta <ohta@parsetree.jp>
2025-05-12 15:48:13 +0900

    initialcommit

--- js/preload-b.js
+++
@@ -1,8 +0,0 @@
-const { contextBridge, ipcRenderer } = require('electron')
-contextBridge.exposeInMainWorld('electronAPI', {
-  onUrl: (callback) => ipcRenderer.on('url', (_event, value) => callback(value)),
-  sendUrl: (url) =>   ipcRenderer.send('url-b', url),
-  sendNavigation: (msg) =>   ipcRenderer.send('nav-b', msg),
-
-})
-
--- js/preload-k2.js
+++
@@ -1,8 +0,0 @@
-const { contextBridge, ipcRenderer } = require('electron')
-contextBridge.exposeInMainWorld('electronAPI', {
-  onUrl: (callback) => ipcRenderer.on('url', (_event, value) => callback(value)),
-  sendUrl: (url) =>   ipcRenderer.send('url-k2', url),
-  sendNavigation: (msg) =>   ipcRenderer.send('nav-k2', msg),
-
-})
-
